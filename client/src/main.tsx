8d327f508f9d32b44b1191a1e0d3e118e843e596
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-07-12 16:20:26 -0400

    chore: remove the design

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 import './main.css';
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
