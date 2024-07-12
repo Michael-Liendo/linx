3fcf76cc7c4fb39c9d5147560317705d24f9f75f
Dariana <daricorreamedina@gmail.com>
2024-07-12 15:22:48 -0500

    Add TailwindCSS into client folder (#8)
    * Add TailwindCSS into client folder
    
    * chore: add package-lock
    
    * chore: change the name
    
    * chore: format code
    
    * chore: remove the design
    
    ---------
    
    Co-authored-by: Michael Liendo <michael.m.liendo.r@gmail.com>

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
+import './main.css';
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 ReactDOM.createRoot(document.getElementById('root')!).render(
