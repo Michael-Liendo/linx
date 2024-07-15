7c96eb1d47719fa8bb983e1b659010affc278e0d
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-07-15 09:19:17 -0400

    chore: utils for th api request

---
+++ client/src/utils/fetch.ts
@@ -0,0 +1,23 @@
+const Fetch = () => {
+  return async (url: string, options?: RequestInit) => {
+    const defaultOptions: { Authorization?: string } = {};
+    const token = '';
+    if (token) {
+      defaultOptions.Authorization = token;
+    }
+    const apiUrl = `${import.meta.env.VITE_API_URL}/${url}`;
+    const requestOptions = {
+      ...options,
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        ...defaultOptions,
+        ...options?.headers,
+      }),
+    };
+    const response = await fetch(apiUrl, requestOptions);
+
+    return response;
+  };
+};
+
+export default Fetch();
