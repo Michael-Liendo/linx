62fb05c74d8c102b75303ef4c71e7fb3ef33c06a
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-07-16 09:20:42 -0400

    chore: me route

--- server/src/routes/user/index.ts
+++ server/src/routes/user/index.ts
@@ -5,12 +5,15 @@ import type {
   RegisterOptions,
 } from 'fastify';
 import me from '../../controllers/User/me';
+import checkJwt from '../../middlewares/checkJwt';
 
 export default function user(
   fastify: FastifyInstance,
   _: RegisterOptions,
   done: DoneFuncWithErrOrRes,
 ) {
+  fastify.register(checkJwt);
+
   fastify.get<{ Reply: IReply }>('/me', me);
 
   done();
