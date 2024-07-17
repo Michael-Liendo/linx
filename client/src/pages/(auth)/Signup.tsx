bce67241df7c4bf72d3aad16faeb706c8649d66f
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-07-17 19:32:53 -0400

    chore: format

--- client/src/pages/(auth)/Signup.tsx
+++ client/src/pages/(auth)/Signup.tsx
@@ -1,5 +1,5 @@
-import { useNavigate } from 'react-router-dom';
 import { type FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Button from '../../components/Button';
 import TextField from '../../components/TextField';
 import useAuth from '../../hooks/Auth';
