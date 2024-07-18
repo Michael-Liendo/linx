ff9bc145bcf95611906d82ce312fab5b9d4aac2f
Daridjcm <daricorreamedina@gmail.com>
2024-07-18 18:27:10 -0500

    Add new components into Home
    - Imported all new components in home.tsx file
    - Add isotype and logotype in the navbar e Layout-Home
    - New component of Button Outline
    - Each function was placed in the components folder
    - Two images were added referring to the visual identity of the Linx project (Isotype and Logo)
    - The width size of the two previous images was changed to ensure good accessibility, behavior and coherence

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,34 +1,10 @@
+import Navbar from '../components/Navbar'
+import Layout from '../components/Layout-Home'
 export default function Home() {
   return (
-    <Navbar />
-  )
-}
-
-function Navbar() {
-  return (
     <>
-      <nav className="p-4 border-b-2 border-blue-500">
-        <div className="container mx-auto flex justify-between items-center">
-          <div className="text-slate-950 text-2xl font-bold">Linx</div>
-          <ul className="flex space-x-4">
-            <li>
-              <a href="#product" className="text-slate-950 hover:text-blue-700">
-                Product
-              </a>
-            </li>
-            <li>
-              <a href="#login" className="text-slate-950 hover:text-blue-700">
-                Login
-              </a>
-            </li>
-            <li>
-              <a href="#signup" className="text-slate-950 hover:text-blue-700">
-                Sign Up
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <Navbar />
+      <Layout />
     </>
-  );
+  )
 }
