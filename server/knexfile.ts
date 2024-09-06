c5204906632b2659bda70ab785de71c0b71201db
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-09-05 23:43:31 -0400

    chore: formatting

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -5,36 +5,36 @@ import type { Knex } from 'knex';
 dotenv.config();
 
 const config: { [key: string]: Knex.Config } = {
-  development: {
-    client: 'pg',
-    connection: {
-      host: process.env.POSTGRES_HOST,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-    },
-    pool: { min: 0, max: 10 },
-  },
-  staging: {
-    client: 'pg',
-    connection: {
-      host: process.env.POSTGRES_HOST,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-    },
-    pool: { min: 0, max: 10 },
-  },
-  production: {
-    client: 'pg',
-    connection: {
-      host: '0.0.0.0',
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-    },
-    pool: { min: 0, max: 10 },
-  },
+	development: {
+		client: 'pg',
+		connection: {
+			host: process.env.POSTGRES_HOST,
+			user: process.env.POSTGRES_USER,
+			password: process.env.POSTGRES_PASSWORD,
+			database: process.env.POSTGRES_DB,
+		},
+		pool: { min: 0, max: 10 },
+	},
+	staging: {
+		client: 'pg',
+		connection: {
+			host: process.env.POSTGRES_HOST,
+			user: process.env.POSTGRES_USER,
+			password: process.env.POSTGRES_PASSWORD,
+			database: process.env.POSTGRES_DB,
+		},
+		pool: { min: 0, max: 10 },
+	},
+	production: {
+		client: 'pg',
+		connection: {
+			host: '0.0.0.0',
+			user: process.env.POSTGRES_USER,
+			password: process.env.POSTGRES_PASSWORD,
+			database: process.env.POSTGRES_DB,
+		},
+		pool: { min: 0, max: 10 },
+	},
 };
 
 module.exports = config;
--- server/migrations/20240712151318_user.ts
+++ server/migrations/20240712151318_user.ts
@@ -1,21 +1,21 @@
 import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
-  await knex.schema.createTable('users', (table) => {
-    table
-      .uuid('id')
-      .unique()
-      .defaultTo(knex.raw('uuid_generate_v4()'))
-      .primary();
-    table.string('first_name').notNullable();
-    table.string('last_name').notNullable();
-    table.string('email').notNullable().unique();
-    table.string('password').notNullable();
-    table.timestamps(true, true);
-  });
+	await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+	await knex.schema.createTable('users', (table) => {
+		table
+			.uuid('id')
+			.unique()
+			.defaultTo(knex.raw('uuid_generate_v4()'))
+			.primary();
+		table.string('first_name').notNullable();
+		table.string('last_name').notNullable();
+		table.string('email').notNullable().unique();
+		table.string('password').notNullable();
+		table.timestamps(true, true);
+	});
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable('users');
+	await knex.schema.dropTable('users');
 }
